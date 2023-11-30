5f0951d08322a594a64435e5df51dd0e58a30cbb
josesantana.web <josesantana.web@gmail.com>
2023-11-30 17:24:22 -0400

    feat: add login page

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,54 +1,98 @@
 // Utils
 
-// Handler
+// Toggle Classes
+// Sirve para cambiar de una clase a otra
+const toggleClasses = (element, class1, class2) => {
+  if (element.classList.contains(class1)) {
+    element.classList.remove(class1);
+    element.classList.add(class2);
+  } else {
+    element.classList.remove(class2);
+    element.classList.add(class1);
+  }
+};
+
+// Click Away
+// Sirve para cerrar un dropdown al hacer click fuera de él
+
+// TODO: Solucionar bug que hace que se cierre el dropdown al hacer click en el handler
+const handleClickAway = (event, handler, dropdown) => {
+  const isClickInside =
+    handler.contains(event.target) || dropdown.contains(event.target);
+
+  if (!isClickInside) {
+    toggleClasses(dropdown, "block", "hidden");
+  }
+
+  // if (!isClickInside) {
+  //   dropdown.classList.remove("block");
+  // }
+};
+
+// Handler Click
+// Sirve para mostrar y ocultar un dropdown al hacer click en un elemento
 const handlerFunction = (handler, element) => {
-  return handler.addEventListener("click", () => {
-    if (element.classList.contains("hidden")) {
-      element.classList.remove("hidden");
-      element.classList.add("block");
-    } else {
-      element.classList.remove("block");
-      element.classList.add("hidden");
-    }
-  });
-}
+  handler?.addEventListener("click", () =>
+    toggleClasses(element, "hidden", "block")
+  );
+};
+
+// Handler Mouseover
+// Sirve para mostrar y ocultar un elemento al pasar el mouse sobre otro
+const handleMouseOver = (sidebarLabel) =>
+  toggleClasses(sidebarLabel, "opacity-0", "opacity-1");
+
+// Handler Mouseout
+// Sirve para mostrar y ocultar un elemento al pasar el mouse sobre otro
+const handleMouseOut = (sidebarLabel) =>
+  toggleClasses(sidebarLabel, "opacity-1", "opacity-0");
 
 // Notifications
 const notificationHandler = document.getElementById("notification-handler");
 const notificationCenter = document.getElementById("notification-center");
 handlerFunction(notificationHandler, notificationCenter);
 
+// document.addEventListener("click", (event) =>
+//   handleClickAway(event, notificationHandler, notificationCenter)
+// );
+
 // Sidebar
 const sidebarHandlers = document.getElementsByClassName("sidebar-handler");
 const sidebarLabels = document.getElementsByClassName("sidebar-label");
 
 [...sidebarHandlers].forEach((sidebarHandler, i) => {
-  let resetTimeout = 0;
-
-  sidebarHandler.addEventListener("mouseover", () => {
-    clearTimeout(resetTimeout);
-    resetTimeout = setTimeout(() => {
-      if (sidebarLabels[i].classList.contains("opacity-0")) {
-        sidebarLabels[i].classList.remove("opacity-0");
-        sidebarLabels[i].classList.add("opacity-1");
-      }
-    }, 500);
-  });
-  sidebarHandler.addEventListener("mouseout", () => {
-    clearTimeout(resetTimeout);
-    if (sidebarLabels[i].classList.contains("opacity-1")) {
-      sidebarLabels[i].classList.remove("opacity-1");
-      sidebarLabels[i].classList.add("opacity-0");
-    }
-  });
+  sidebarHandler.addEventListener("mouseover", () =>
+    handleMouseOver(sidebarLabels[i])
+  );
+  sidebarHandler.addEventListener("mouseout", () =>
+    handleMouseOut(sidebarLabels[i])
+  );
 });
 
 // Account
 const accountHandler = document.getElementById("account-handler");
 const accountDropdown = document.getElementById("account-dropdown");
+
 handlerFunction(accountHandler, accountDropdown);
+
+// document.addEventListener("click", (event) =>
+//   handleClickAway(event, accountHandler, accountDropdown)
+// );
 
 // Status Dropdown
 const statusHandler = document.getElementById("status-handler");
 const statusDropdown = document.getElementById("status-dropdown");
+
 handlerFunction(statusHandler, statusDropdown);
+// document.addEventListener("click", (event) =>
+//   handleClickAway(event, statusHandler, statusDropdown)
+// );
+
+// Language Dropdown
+const languageHandler = document.getElementById("language-handler");
+const languageDropdown = document.getElementById("language-dropdown");
+
+handlerFunction(languageHandler, languageDropdown);
+// document.addEventListener("click", (event) =>
+//   handleClickAway(event, languageHandler, languageDropdown)
+// );
